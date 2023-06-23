24bb78ee8f104dcaee1db2ab58cb09e71f9d32bc
ildar <ildar.gazizov2607@mail.ru>
2023-06-23 20:49:55 +0400

    Docker path requires name of container not the "localhost"

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 engine = create_engine(
-    'postgresql://postgres:123@pythonproject-db-1/example', # Ваши настройки postgres`а
+    'postgresql://postgres:123@db/example', # Ваши настройки postgres`а
     echo=True
 )
 Base = declarative_base()
